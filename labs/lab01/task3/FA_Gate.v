85ef320ce31b74092e55f496ac1552508dca6b48
Adhvikji <f20251300@pilani.bits-pilani.ac.in>
2026-08-30 20:27:13 +0000

    [Classroom 50] Submit dd-2026-verilog-labs

--- labs/lab01/task3/FA_Gate.v
+++ labs/lab01/task3/FA_Gate.v
@@ -26,10 +26,10 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  xor #(2,3) (ps,  a,   b);
-  and #(3,2) (pc1, a,   b);
-  xor #(1,1) (sum, cin, ps);
-  and #(1,2) (pc2, cin, ps);
-  or  #(2,4) (cout, pc1, pc2);
+  xor #(2) (ps,  a,   b);
+  and #(3) (pc1, a,   b);
+  xor #(1) (sum, cin, ps);
+  and #(1) (pc2, cin, ps);
+  or  #(2) (cout, pc1, pc2);
 
 endmodule
